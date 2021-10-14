77b5fc89b0718143b3b74879876ec51bb1e370ef
Jaxongir <jmuxutdinov@mail.ru>
2021-10-14 16:46:32 +0500

    new lessons

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import  Hooks  from './Hooks'
+import  Old from './Old'
+export const App = () => {
+  return (
+    <div>
+      {/* <Hooks/> */}
+      <Old/>
+    </div>
+  )
+}
---
+++ src/Old.js
@@ -0,0 +1,37 @@
+// import React from "react";
+// import Hooks from "./Hooks";
+// class Old extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       counter: 0,
+//       status: "student",
+//     };
+//   }
+//   render() {
+//     return (
+//       <div>
+//         Old <br />
+//         <input
+//           placeholder="status"
+//           value={this.state.status}
+//           onChange={(e) => this.setState({ status: e.target.value })}
+//         />
+//         <button
+//           onClick={() => this.setState({ counter: this.state.counter + 1 })}
+//         >
+//           +
+//         </button>
+//         {this.state.counter}
+//         <button
+//           onClick={() => this.setState({ counter: this.state.counter - 1 })}
+//         >
+//           -
+//         </button>
+//         <hr />
+//         <Hooks count={this.state.counter} text={this.state.status} />
+//       </div>
+//     );
+//   }
+// }
+// export default Old;
