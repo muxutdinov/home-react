80a770acfc4bfdc66cc5862fc83236c280e207ad
Jaxongir <jmuxutdinov@mail.ru>
2021-10-15 10:00:09 +0500

    no commint

--- src/Hooks.js
+++ src/Hooks.js
@@ -1,30 +1,30 @@
-// import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
-// const Hooks = (props) => {
-//   const [counter, setCounter] = useState(props.count);
-//   const [data, setData] = useState(props.text);
+const Hooks = (props) => {
+  const [counter, setCounter] = useState(props.count);
+  const [data, setData] = useState(props.text);
 
-//   useEffect(()=>{
-//       setCounter(props.count)
-//       console.log("Hooks clicked");
-//   },[props.count])
+  useEffect(()=>{
+      setCounter(props.count)
+      console.log("Hooks clicked");
+  },[props.count])
   
-//   useEffect(()=>{
-//     setData(props.text)
-// })
-//   return (
-//     <div>
-//       Hooks <br />
-//       <input
-//         placeholder="status"
-//         value={data}
-//         onChange={(e) => setData(props.text)}
-//       />
-//       <button onClick={() => setCounter(counter + 1)}>+</button>
-//       {counter}
-//       <button onClick={() => setCounter(counter - 1)}>-</button>
-//       <hr />
-//     </div>
-//   );
-// };
-// export default Hooks;
+  useEffect(()=>{
+    setData(props.text)
+})
+  return (
+    <div>
+      Hooks <br />
+      <input
+        placeholder="status"
+        value={data}
+        onChange={(e) => setData(props.text)}
+      />
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+      {counter}
+      <button onClick={() => setCounter(counter - 1)}>-</button>
+      <hr />
+    </div>
+  );
+};
+export default Hooks;
