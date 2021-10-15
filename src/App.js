6a7139949ccc80ebafd25a0664936af1f34ec306
Jaxongir <jmuxutdinov@mail.ru>
2021-10-15 11:25:50 +0500

    go live

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 // import  Hooks  from './Hooks'
 // import  Old from './Old'
+import  Navbar  from './Navbar'
 import MuviList from './MuviList'
 export const App = () => {
   return (
     <div>
+      <Navbar/>
      <MuviList/>
     </div>
   )
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Container, Item } from './NavbarStyle'
 
 export const Navbar = () => {
     return (
-        <Container>
-            <Item>MovieList</Item>
-            <Item>{}</Item>
-        </Container>
+       <div>
+              <h1>MovieList</h1>
+              <hr/>
+       </div>
     )
 }
+export default Navbar
